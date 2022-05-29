e70319ad8eb5dd141196fed193f1edf19b820950
RubyYeung <77617862+RubyYeung@users.noreply.github.com>
2022-05-29 11:51:04 +0800

    Create TestTicTacToe.java

---
+++ TestTicTacToe.java
@@ -0,0 +1,9 @@
+public class TestTicTacToe {
+    public static void main(String args[]){
+    TicTacToe ticTacToe = new TicTacToe();
+    ticTacToe.printBoard();
+    //ticTacToe.playerMove();
+    //ticTacToe.computerRandomMove();
+    ticTacToe.play();
+    }
+}
